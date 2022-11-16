d935a55f8531b16cb23dbefb3a5b49b6dacf72dd
12e21 <2496813401@qq.com>
2022-11-17 00:11:22 +0800

    添加了分支语句的笔记

--- scripts/judge_number.py
+++ scripts/judge_number.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-print('请输入一个整数:')
-input_num=int(input())
-if input_num > 0:
-    print('它是一个正数')
-elif input_num==0:
-    print('它是零')
-else:
-    print('它是一个负数')
+while True:
+    print('请输入一个实数:')
+    input_num=float(input())
+    if input_num > 0:
+        print('它是一个正数')
+        if (input_num-int(input_num)==0):
+            print('它是一个整数')
+            multi_count=input_num
+            multi_num=input_num
+            while multi_count >=2:
+                multi_num*=(multi_count-1)
+                multi_count-=1
+            print(f'它的阶乘是{multi_num}')
+        else:
+            print('它不是整数')
+    elif input_num==0:
+        print('它是零')
+    else:
+        print('它是一个负数')
+    print('\n')
