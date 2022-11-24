072f1b5cc4fcc648e78ba1e3ab9048ec5c6ded97
12e21 <2496813401@qq.com>
2022-11-24 19:20:33 +0800

    xor破解方法

--- scripts/secret/encrypto_lib.py
+++ scripts/secret/encrypto_lib.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
-
-def encrypto(plaintext,bias):
+#此函数用于偏移式加密/解密
+def encrypto_bias(plaintext,bias):
     plaintext_list=list(plaintext)
     plainnumber_list=[ord(char) for char in plaintext_list]
     plainnumber_bias_list=[num+bias for num in plainnumber_list]
     ciphertext_list=[chr(num) for num in plainnumber_bias_list]
     ciphertext=''.join(ciphertext_list)
     return ciphertext
-def unencrypto(ciphertext,bias):
-    ciphertext_list=list(ciphertext)
-    ciphernumber_list=[ord(char) for char in ciphertext_list]
-    ciphernumber_bias_list=[num-bias for num in ciphernumber_list]
-    plaintext_list=[chr(num) for num in ciphernumber_bias_list]
-    plaintext=''.join(plaintext_list)
-    return plaintext
+#此函数用于异或式加密解密(单字符密钥)
+def encrypto_xor(plaintext,key):
+    plaintext_list=list(plaintext)
+    plainnumber_list=[ord(char) for char in plaintext_list]
+    plainnumber_xor_list=[num^key for num in plainnumber_list]
+    ciphertext_list = [chr(num) for num in plainnumber_xor_list]
+    ciphertext = ''.join(ciphertext_list)
+    return ciphertext
+
+#本函数用于破解xor加密文本
+def crack_xor(cipher_text,crack_key_count):
+    cipher_list=list(cipher_text)
+    ciphernumber_list=[ord(char) for char in cipher_list]
+    for key in range(crack_key_count):
+        ciphernumber_xor_list=[num^key for num in ciphernumber_list]
+        plaintext_list=[chr(num) for num in ciphernumber_xor_list]
+        plaintext=''.join(plaintext_list)
+        print(f'密钥{key}: {plaintext}')
+    print('\n破解已完成')
+
+
+
+
 if __name__=='__main__':
     print('加密模块正在运行\n')
 else:
